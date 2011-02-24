411beb259604094c4aa9b9a6d086f6ce5ec97124
James Cotton <peabody124@gmail.com>
2011-02-24 01:21:19 -0600

    Change elif to else

--- arch/arm/plat-omap/cpu-omap.c
+++ arch/arm/plat-omap/cpu-omap.c
@@ -136,7 +136,7 @@ static int __init omap_cpu_init(struct cpufreq_policy *policy)
 
 #ifdef CONFIG_MACH_OMAP3621_EVT1A
         policy->cur = policy->min = policy->max = 800000;
-#elif
+#else
     policy->cur = policy->min = policy->max = omap_getspeed(0);
 #endif
 
@@ -157,7 +157,7 @@ static int __init omap_cpu_init(struct cpufreq_policy *policy)
 #ifdef CONFIG_MACH_OMAP3621_EVT1A
 	policy->max = 800000;
 	policy->cur = 800000;
-#elif
+#else
 	policy->max = policy->cpuinfo.max_freq;
 	policy->cur = omap_getspeed(0);
 #endif
